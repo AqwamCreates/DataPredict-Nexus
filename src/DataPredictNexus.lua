94518daab18dd2034f23aeb32bf0b04bf9579a68
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-03-27 08:17:33 +0800

    Update DataPredictNexus.lua

--- src/DataPredictNexus.lua
+++ src/DataPredictNexus.lua
@@ -10,6 +10,10 @@ local defaultPort = 4444
 
 local defaultSyncTime = 3 * 60
 
+local defaultNumberOfSyncRetry = 3
+
+local defaultSyncRetryDelay = 2
+
 local logTypeArray = {"Error", "Warning"}
 
 local DataPredictNexusInstancesArray = {}
@@ -36,8 +40,14 @@ function DataPredictNexus.new(propertyTable: {})
 	
 	local syncTime: number = propertyTable.syncTime or defaultSyncTime
 	
+	local numberOfSyncRetry = propertyTable.numberOfSyncRetry or defaultNumberOfSyncRetry
+	
+	local syncRetryDelay = propertyTable.syncRetryDelay or defaultSyncRetryDelay
+	
 	if (not encryptionKey) then warn("Without an encryption key, the data will not be encrypted. This means that the hackers can intercept the unencrypted data.") end
-
+	
+	local commandFunctionArray = {}
+	
 	local logArray = {}
 
 	local syncThread = nil
@@ -62,13 +72,27 @@ function DataPredictNexus.new(propertyTable: {})
 		
 	end
 	
-	local function onSucessfulSync(responseBody)
+	local function onCommandReceived(command, value)
 		
+		local commandFunction = commandFunctionArray[command]
 		
+		if (not commandFunction) then addLog("Warning", "Command function for " .. command .. " does not exist.") return end
+		
+		commandFunction(value)
 		
 	end
 	
-	local function onSync()
+	local function processResponseDictionary(responseDictionary)
+		
+		for command, value in responseDictionary do
+			
+			onCommandReceived(command, value)
+			
+		end
+		
+	end
+	
+	local function fetchResponseDictionary()
 		
 		local addressWithPort = address .. ":" .. port
 		
@@ -80,11 +104,25 @@ function DataPredictNexus.new(propertyTable: {})
 		
 		local requestBody = HttpService:JSONEncode(requestDictionary)
 		
-		local success, responseBody = pcall(function() return HttpService:PostAsync(addressWithPort, requestBody, Enum.HttpContentType.ApplicationJson) end)
-
-		if (not success) then addLog("Error", "Unable to fetch response from " .. addressWithPort .. ".") return end
+		local currentSyncRetryDelay = syncRetryDelay
 		
-		onSucessfulSync(responseBody)
+		for attempt = 1, numberOfSyncRetry, 1 do
+			
+			local success, responseBody = pcall(function() return HttpService:PostAsync(addressWithPort, requestBody, Enum.HttpContentType.ApplicationJson) end)
+			
+			if success then return HttpService:JSONDecode(responseBody) end
+			
+			addLog("Warning", "Sync attempt " .. attempt .. " failed. Retrying in " .. syncRetryDelay .. " seconds.")
+			
+			task.wait(currentSyncRetryDelay)
+			
+			currentSyncRetryDelay = currentSyncRetryDelay * 2 -- Exponential backoff
+			
+		end
+		
+		addLog("Warning", "Unable to fetch response from " .. addressWithPort .. ".")
+		
+		return nil
 		
 	end
 	
@@ -96,7 +134,9 @@ function DataPredictNexus.new(propertyTable: {})
 			
 			while true do
 				
-				onSync()
+				local responseDictionary = fetchResponseDictionary()
+				
+				if responseDictionary then processResponseDictionary(responseDictionary) end
 				
 				task.wait(syncTime)
 				
@@ -116,6 +156,18 @@ function DataPredictNexus.new(propertyTable: {})
 		
 	end
 	
+	local function addCommand(commandName, functionToRun)
+		
+		commandFunctionArray[commandName] = functionToRun
+		
+	end
+	
+	local function removeCommand(commandName)
+		
+		commandFunctionArray[commandName] = nil
+		
+	end
+	
 	return {
 		
 		addLog = addLog,
@@ -123,11 +175,13 @@ function DataPredictNexus.new(propertyTable: {})
 		clearAllLogs = clearAllLogs,
 		
 		startSync = startSync,
-		stopSync = stopSync
+		stopSync = stopSync,
+		
+		addCommand = addCommand,
+		removeCommand = removeCommand
 		
 	}
 	
 end
-
 
 return DataPredictNexus
